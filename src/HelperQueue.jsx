96269e8a66ef6d05194acba370765227aa2cbaf1
Omar Diab <o.s.diab@gmail.com>
2014-11-07 13:06:10 -0800

    add sample components for each route

---
+++ src/HelperQueue.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+module.exports = React.createClass({
+    render: function() {
+        return (
+            <div classname="helper-queue">
+                <p>Hello World!</p>
+            </div>
+        );
+    }
+});
