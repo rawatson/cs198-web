73b6f5b1aa448af3ad8c9f0c4bf85fcd9298cd57
Omar Diab <o.s.diab@gmail.com>
2014-11-07 13:06:22 -0800

    add basic routing to application

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,41 @@
-var React   = require('react');
+// React/routing dependencies
+var React =             require('react');
+var Router =            require('react-router');
+var Route =             Router.Route;
+var Routes =            Router.Routes;
+var NotFoundRoute =     Router.NotFoundRoute;
+var DefaultRoute =      Router.DefaultRoute;
+var Link =              Router.Link;
 
-React.renderComponent(
-    <div>Hello world!</div>,
-    document.body
+// Components
+var HelperQueue =   require('./HelperQueue.jsx');
+var SignUp =        require('./SignUp.jsx');
+var NotFound =      require('./NotFound.jsx');
+
+var App = React.createClass({
+    // TODO: don't expose links to go between each view; password protect
+    render: function() {
+        return (
+            <div>
+                <ul>
+                    <li><Link to="queue">Queue</Link></li>
+                    <li><Link to="signup">Sign Up</Link></li>
+                </ul>
+                <this.props.activeRouteHandler />
+            </div>
+        );
+    }
+});
+
+var routes = (
+    <Routes location="history">
+        <Route name="app" path="/" handler={App}>
+            <Route name="queue" handler={HelperQueue} />
+            <Route name="signup" handler={SignUp} />
+            <DefaultRoute handler={NotFound} />
+        </Route>
+        <NotFoundRoute handler={NotFound} />
+    </Routes>
 );
+
+React.renderComponent(routes, document.body);
