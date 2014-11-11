cdc4486d2d3b9dfea6f8f51eff54e692f5d27945
Omar Diab <o.s.diab@gmail.com>
2014-11-10 20:12:37 -0800

    implement signup flow without AJAX requests

--- src/SignupBox.jsx
+++ src/SignupBox.jsx
@@ -1,10 +1,66 @@
 var React = require('react');
+var SignupForm = require('./SignupForm.jsx');
+var ActiveHelpers = require('./ActiveHelpers.jsx');
 
 module.exports = React.createClass({
+    getInitialState: function() {
+        return {student: null};
+    },
+    resetForm: function() {
+        this.setState({student: null});
+    },
+    onHelpRequest: function(student, queueStats) {
+        this.setState({student: student, queueStats: queueStats});
+    },
+    positionMessage: function(pos) {
+        switch (pos) {
+        case 0:
+            return "You are next to receive help!";
+        case 1:
+            return "There is one person ahead of you.";
+        default:
+            return "There are " + pos + " people ahead of you.";
+        }
+    },
     render: function() {
+        var content;
+        if (this.state.student === null) {
+            content = (
+                <div>
+                    <h1>Welcome to the LaIR</h1>
+                    <p>Enter your SUnetID to request or view the status of your request.</p>
+                    <SignupForm submitCallback={this.onHelpRequest} />
+                </div>
+            );
+        } else {
+            // reset the form after delay
+            // TODO: make the timeout visible
+            var formResetTimeout = 10000;
+            var timer = setTimeout(this.resetForm, formResetTimeout);
+            var resetForm = this.resetForm;
+
+            // Click handler needs to cancel the resetForm event
+            var clickHandler = function(timer) {
+                return function() {
+                    clearTimeout(timer);
+                    resetForm();
+                };
+            }(timer);
+
+            content = (
+                <div>
+                    <h1>{this.state.student.first_name}, we have your help request!</h1>
+                    <p>{this.positionMessage(this.state.queueStats.position)}</p>
+                    <p>We will do our best to get to you as quickly as possible.</p>
+                    <button onClick={clickHandler}>Return to request help</button>
+                </div>
+            );
+        }
         return (
-            <div classname="helper-queue">
-                <p>Hola, El Mundo!</p>
+            <div className="signup-box">
+                {content}
+                <h2>Current LaIR helpers</h2>
+                <ActiveHelpers />
             </div>
         );
     }
