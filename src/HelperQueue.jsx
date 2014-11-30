673c9474b8fe3fd6c3b8eb18cd5c4e0a93044cd3
Omar Diab <o.s.diab@gmail.com>
2014-11-30 11:31:03 -0800

    Reduce refresh timeout to 5000

--- src/HelperQueue.jsx
+++ src/HelperQueue.jsx
@@ -39,7 +39,7 @@ module.exports = React.createClass({
         };
     },
     componentDidMount: function() {
-        setTimeout(this.refreshState, 10000);
+        setTimeout(this.refreshState, 5000);
         this.refreshState();
     },
     render: function() {
