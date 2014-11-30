06ec6a968672926902ea846b4c490e4f5f0c11a9
Omar Diab <o.s.diab@gmail.com>
2014-11-30 14:56:44 -0800

    implement active helpers panel

--- src/ActiveHelpers.jsx
+++ src/ActiveHelpers.jsx
@@ -1,11 +1,52 @@
 var React   = require('react');
+var Api     = require('./Api');
 var _       = require('underscore');
 
 module.exports = React.createClass({
+    handleSignIn: function(e) {
+        e.preventDefault();
+
+        data = {
+            person_id: this.refs.sunet_id.getDOMNode().value
+        };
+        Api.Helpers.checkin(data).then(function(helper) {
+            var helpers = this.props.helpers;
+
+            // Don't add immediately; checkin is idempotent, so it succeeds if the helper is
+            // already checked in.
+            var notPresent = (_.find(helpers, function(h) {
+                return h.id == helper.id;
+            }) === undefined);
+            if (notPresent) {
+                helpers = _.clone(this.props.helpers);
+                helpers.push(helper);
+            }
+
+            this.props.refresh(helpers);
+        }.bind(this), function(err) {
+            // TODO: real error handling
+            alert(JSON.stringify(err));
+        });
+    },
+    handleSignOut: function(helper_id, e) {
+        data = {
+            person_id: helper_id
+        };
+
+        Api.Helpers.checkout(data).then(function() {
+            this.props.refresh(_.reject(this.props.helpers, function(h) {
+                return h.id == helper_id;
+            }));
+        }.bind(this), function(err) {
+            // TODO: real error handling
+            alert(JSON.stringify(err));
+        });
+    },
     renderHelper: function(helper) {
         return (
             <li>
-                <button className="active-helpers-sign-out">Sign out</button>
+                <button className="active-helpers-sign-out"
+                        onClick={this.handleSignOut.bind(this, helper.id)}>Sign out</button>
                 <span className="active-helpers-name">{helper.first_name} {helper.last_name}</span>
             </li>
         );
@@ -20,8 +61,8 @@ module.exports = React.createClass({
                 <ul>
                     {_.map(this.props.helpers, this.renderHelper)}
                     <li>
-                        <form>
-                            <input type="text" placeholder="SUNet ID" />
+                        <form onSubmit={this.handleSignIn}>
+                            <input type="text" ref="sunet_id" placeholder="SUNet ID" />
                             <input type="submit" value="Sign in" />
                         </form>
                     </li>
