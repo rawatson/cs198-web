8da82d965d41cc828b23dbbffa634e00817217c4
Omar Diab <o.s.diab@gmail.com>
2014-11-30 20:36:49 -0800

    implement help request assignment and students leaving

--- src/HelpRequest.jsx
+++ src/HelpRequest.jsx
@@ -2,7 +2,34 @@ var React   = require('react');
 var _       = require('underscore');
 var moment  = require("moment");
 
+var Api = require("./Api");
+
 module.exports = React.createClass({
+    getInitialState: function() {
+        return {hidden: false};
+    },
+    resolveRequest: function(reason) {
+        e.preventDefault();
+
+        Api.HelpRequests.resolve(this.props.request.id, reason).then(function() {
+            this.setState({hidden: true});
+            this.props.refresh();
+        }.bind(this), function(err) {
+            // TODO: handle error
+            alert(err);
+        });
+    },
+    assignRequest: function() {
+        var helper_id = parseInt(this.refs.helper.getDOMNode().value);
+        Api.HelpRequests.assign(this.props.request.id, helper_id).then(function() {
+            this.setState({hidden: true});
+            this.props.refresh();
+        }.bind(this), function(err) {
+            // TODO: handle error
+            alert(err);
+        });
+
+    },
     renderHelperOption: function(h) {
         return <option value={h.id}>{h.person.first_name} {h.person.last_name}</option>;
     },
@@ -26,8 +53,8 @@ module.exports = React.createClass({
             </span>,
         ];
 
-        if (request.helper_assignment) {
-            var helper = request.helper_assignment.helper.person;
+        if (request.helper) {
+            var helper = request.helper.person;
             elems.push(
                 <span className="request-assignment">
                     Assigned to {helper.first_name} {helper.last_name}
@@ -37,19 +64,40 @@ module.exports = React.createClass({
             var availableHelpers = _.filter(this.props.helpers, function(h) {
                 return !h.help_request;
             });
-            elems.push(<button onClick={this.markLeft}>Student left</button>);
-            elems.push(
-                <form onSubmit={this.assignRequest}>
-                    <input type="submit">Assign to...</input>
-                    <select ref="helper">
-                        {_.map(availableHelpers, this.renderHelperOption)}
-                    </select>
-                </form>
-            );
+
+            var assignElem;
+            if (_.isEmpty(availableHelpers)) {
+                assignElem = <span>Cannot assign; all helpers busy.</span>;
+            } else {
+                var assignRequestHandler = function(e) {
+                    e.preventDefault();
+                    this.assignRequest();
+                }.bind(this);
+                assignElem = (
+                    <form onSubmit={assignRequestHandler}>
+                        <input type="submit" value="Assign to..." />
+                        <select ref="helper">
+                            {_.map(availableHelpers, this.renderHelperOption)}
+                        </select>
+                    </form>
+                );
+            }
+
+            var resolveRequestHandler = function(e) {
+                e.preventDefault();
+                this.resolveRequest("left");
+            }.bind(this);
+
+            elems.push(<button onClick={resolveRequestHandler}>Student left</button>);
+            elems.push(assignElem);
         }
 
+        var className = "help-request";
+        if (this.state.hidden) {
+            className += " hidden";
+        }
         return (
-            <div class="help-request">
+            <div class={className}>
                 {elems}
             </div>
         );
