fb6cfa38e7b3b9b9470827380f3d2791eeadab17
Omar Diab <o.s.diab@gmail.com>
2014-11-30 17:39:13 -0800

    rework help request api for testing purposes

--- src/Api.js
+++ src/Api.js
@@ -1,4 +1,5 @@
-var _ = require("underscore");
+var _       = require("underscore");
+var moment  = require("moment");
 
 var lairState = { enabled: true };
 var helpers = [
@@ -26,6 +27,54 @@ var helpers = [
       sunet_id: "maesenc",
       active: false
     }
+];
+var helpRequests = [
+    { id: 1,
+      helper_assignment: {
+        helper: { person: {
+            first_name: "Elmer",
+            last_name: "Le",
+            sunet_id: "elmerle" } }
+      },
+      person: {
+          first_name: "Daniel",
+          last_name: "Steffee"
+      },
+      position: 0,
+      created_at: "May 2 2014",
+      course: {
+          code: "CS106A",
+      }, description: "It broked", location: "19" },
+    { id: 2,
+      person: {
+          first_name: "Robert",
+          last_name: "Harlow"
+      },
+      position: 1,
+      created_at: "May 3 2014",
+      course: {
+          code: "CS106A",
+      }, description: "It really broked", location: "9" },
+    { id: 3,
+      person: {
+          first_name: "Amy",
+          last_name: "Nguyen"
+      },
+      position: 2,
+      created_at: "May 4 2014",
+      course: {
+          code: "CS106A",
+      }, description: "It broked so hard", location: "23" },
+    { id: 4,
+      person: {
+        first_name: "Mehran",
+        last_name: "Sahami"
+      },
+      course: {
+          code: "CS106A"
+      },
+      close_status: "left", created_at: moment().subtract(10, "minutes"),
+      description: "It broked but I'm out", location: "2" }
 ];
 
 // TODO: actually send AJAX requests
@@ -92,48 +141,36 @@ module.exports = {
     },
     HelpRequests: {
         find: function(opts) {
-            if (typeof opts === "undefined") opts = {};
+            if (typeof opts === "undefined") opts = {unassigned: true};
+            console.log(JSON.stringify(opts));
 
+            var filters = [];
             if (opts.unassigned) {
-                return new Promise(function(resolve, reject) {
-                    var requests = [
-                        { id: 1,
-                          person: {
-                              first_name: "Omar",
-                              last_name: "Diab"
-                          },
-                          course: {
-                              code: "CS106A",
-                          }, description: "It broked", location: "19" },
-                        { id: 2,
-                          person: {
-                              first_name: "Robert",
-                              last_name: "Harlow"
-                          },
-                          course: {
-                              code: "CS106A",
-                          }, description: "It really broked", location: "9" },
-                        { id: 3,
-                          person: {
-                              first_name: "Amy",
-                              last_name: "Nguyen"
-                          },
-                          course: {
-                              code: "CS106A",
-                          }, description: "It broked so hard", location: "23" },
-                    ];
-                    resolve(requests);
+                filters.push(function(r) { return !r.helper_assignment; });
+            } else if (opts.unassigned === false) {
+                filters.push(function(r) { return !!r.helper_assignment; });
+            }
+
+            if (opts.closed) {
+                filters.push(function(r) { return !!r.close_status; });
+            } else {
+                filters.push(function(r) { return !r.close_status; });
+            }
+
+            if (typeof opts.since !== "undefined") {
+                filters.push(function(r) {
+                    return moment(r.created_at).isAfter(moment(opts.since));
                 });
             }
 
-            if (typeof opts.left_before !== "undefined") {
-                return new Promise(function(resolve, reject) {
-                    resolve([
-                        { id: 0, person_id: 0, course_id: 1,
-                            description: "It broked but I'm out", location: "2" },
-                    ]);
-                });
-            }
+            return new Promise(function(resolve, reject) {
+                var curr = helpRequests;
+                for (var filter in filters) {
+                    curr = _.filter(curr, filters[filter]);
+                }
+
+                resolve(curr);
+            });
         }
     }
 };
