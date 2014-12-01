07b4852c3fd2aa53a80285117563dc51021e45c0
Omar Diab <o.s.diab@gmail.com>
2014-12-01 01:06:40 -0800

    simplify by using assignhelperform

--- src/HelpRequest.jsx
+++ src/HelpRequest.jsx
@@ -3,26 +3,21 @@ var _       = require('underscore');
 var moment  = require("moment");
 
 var Api = require("./Api");
+var AssignHelperForm = require("./AssignHelperForm.jsx");
 
 module.exports = React.createClass({
-    getInitialState: function() {
-        return {hidden: false};
-    },
     resolveRequest: function(reason) {
-        e.preventDefault();
-
         Api.HelpRequests.resolve(this.props.request.id, reason).then(function() {
-            this.setState({hidden: true});
             this.props.refresh();
         }.bind(this), function(err) {
             // TODO: handle error
             alert(err);
         });
     },
-    assignRequest: function() {
-        var helper_id = parseInt(this.refs.helper.getDOMNode().value);
-        Api.HelpRequests.assign(this.props.request.id, helper_id).then(function() {
-            this.setState({hidden: true});
+    reassignRequest: function(helper_id) {
+        if (typeof helper_id === "string") helper_id = parseInt(helper_id);
+
+        Api.HelpRequests.reassign(this.props.request.id, helper_id).then(function() {
             this.props.refresh();
         }.bind(this), function(err) {
             // TODO: handle error
@@ -30,74 +25,82 @@ module.exports = React.createClass({
         });
 
     },
-    renderHelperOption: function(h) {
-        return <option value={h.id}>{h.person.first_name} {h.person.last_name}</option>;
+    assignRequest: function(helper_id) {
+        if (typeof helper_id === "string") helper_id = parseInt(helper_id);
+
+        Api.HelpRequests.assign(this.props.request.id, helper_id).then(function() {
+            this.props.refresh();
+        }.bind(this), function(err) {
+            // TODO: handle error
+            alert(err);
+        });
+
+    },
+    availableHelpers: function() {
+        return _.filter(this.props.helpers, function(h) {
+            return !h.help_request;
+        });
     },
     render: function() {
         var request = this.props.request;
+
         var elems = [
-            <span className="request-student">
-                {request.person.first_name} {request.person.last_name}
-            </span>,
-            <span className="request-description">
-                {request.description}
-            </span>,
-            <span className="request-course">
-                {request.course.code}
-            </span>,
-            <span className="request-location">
-                {request.location}
-            </span>,
-            <span className="request-timestamp">
-                {moment(request.created_at).format("h:mm A MMM D, YYYY")}
-            </span>,
+            <div className="request-info">
+                <span className="request-student">
+                    {request.person.first_name} {request.person.last_name}
+                </span>
+                <span className="request-description">
+                    {request.description}
+                </span>
+                <span className="request-course">
+                    {request.course.code}
+                </span>
+                <span className="request-location">
+                    {request.location}
+                </span>
+                <span className="request-timestamp">
+                    {moment(request.created_at).format("h:mm A MMM D, YYYY")}
+                </span>
+            </div>
         ];
 
         if (request.helper) {
             var helper = request.helper.person;
+            var resolveRequestHandler = function(reason, e) {
+                e.preventDefault();
+                this.resolveRequest("resolved");
+            };
+
             elems.push(
                 <span className="request-assignment">
                     Assigned to {helper.first_name} {helper.last_name}
-                </span>
-            );
+                </span>);
+            elems.push(
+                <button onClick={resolveRequestHandler.bind(this, "left")}>Student left</button>);
+            elems.push(
+                <button onClick={resolveRequestHandler.bind(this, "resolved")}>Resolved</button>);
+            elems.push(<AssignHelperForm
+                availableHelpers={this.availableHelpers()}
+                callback={this.reassignRequest}
+                prompt="Reassign to..." verb="reassign" />);
         } else {
-            var availableHelpers = _.filter(this.props.helpers, function(h) {
-                return !h.help_request;
-            });
-
-            var assignElem;
-            if (_.isEmpty(availableHelpers)) {
-                assignElem = <span>Cannot assign; all helpers busy.</span>;
+            var prompt;
+            var verb;
+            if (this.props.request.close_status) {
+                prompt = "Reopen and assign to...";
+                verb = "reopen";
             } else {
-                var assignRequestHandler = function(e) {
-                    e.preventDefault();
-                    this.assignRequest();
-                }.bind(this);
-                assignElem = (
-                    <form onSubmit={assignRequestHandler}>
-                        <input type="submit" value="Assign to..." />
-                        <select ref="helper">
-                            {_.map(availableHelpers, this.renderHelperOption)}
-                        </select>
-                    </form>
-                );
+                prompt = "Assign to...";
+                verb = "assign";
             }
-
-            var resolveRequestHandler = function(e) {
-                e.preventDefault();
-                this.resolveRequest("left");
-            }.bind(this);
-
-            elems.push(<button onClick={resolveRequestHandler}>Student left</button>);
-            elems.push(assignElem);
+            elems.push(<AssignHelperForm
+                availableHelpers={this.availableHelpers()}
+                callback={this.assignRequest}
+                prompt={prompt} verb={verb} />);
         }
 
-        var className = "help-request";
-        if (this.state.hidden) {
-            className += " hidden";
-        }
         return (
-            <div class={className}>
+            <div class="help-request">
                 {elems}
             </div>
         );
