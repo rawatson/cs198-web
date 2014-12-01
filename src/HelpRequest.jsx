7df6e1b2d026cecf27e152190d377f776d4628ef
Omar Diab <o.s.diab@gmail.com>
2014-11-30 18:01:59 -0800

    add ability to display helper assignment name

--- src/HelpRequest.jsx
+++ src/HelpRequest.jsx
@@ -5,23 +5,36 @@ var moment  = require("moment");
 module.exports = React.createClass({
     render: function() {
         var request = this.props.request;
+        var elems = [
+            <span className="request-student">
+                {request.person.first_name} {request.person.last_name}
+            </span>,
+            <span className="request-description">
+                {request.description}
+            </span>,
+            <span className="request-course">
+                {request.course.code}
+            </span>,
+            <span className="request-location">
+                {request.location}
+            </span>,
+            <span className="request-timestamp">
+                {moment(request.created_at).format("h:mm A MMM D, YYYY")}
+            </span>,
+        ];
+
+        if (request.helper_assignment) {
+            var helper = request.helper_assignment.helper.person;
+            elems.push(
+                <span className="request-assignment">
+                    Assigned to {helper.first_name} {helper.last_name}
+                </span>
+            );
+        }
+
         return (
             <div class="help-request">
-                <span className="request-student">
-                    {request.person.first_name} {request.person.last_name}
-                </span>
-                <span className="request-description">
-                    {request.description}
-                </span>
-                <span className="request-course">
-                    {request.course.code}
-                </span>
-                <span className="location">
-                    {request.location}
-                </span>
-                <span className="timestamp">
-                    {moment(request.created_at).format("h:mm A MMM D, YYYY")}
-                </span>
+                {elems}
             </div>
         );
     }
