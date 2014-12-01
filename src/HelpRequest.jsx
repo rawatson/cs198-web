62bdc80710d8a4d32083bfee59a1f6d52bf2f4bc
Omar Diab <o.s.diab@gmail.com>
2014-11-30 19:56:30 -0800

    display interactions with requests in help requests

--- src/HelpRequest.jsx
+++ src/HelpRequest.jsx
@@ -3,6 +3,9 @@ var _       = require('underscore');
 var moment  = require("moment");
 
 module.exports = React.createClass({
+    renderHelperOption: function(h) {
+        return <option value={h.id}>{h.person.first_name} {h.person.last_name}</option>;
+    },
     render: function() {
         var request = this.props.request;
         var elems = [
@@ -29,6 +32,19 @@ module.exports = React.createClass({
                 <span className="request-assignment">
                     Assigned to {helper.first_name} {helper.last_name}
                 </span>
+            );
+        } else {
+            var availableHelpers = _.filter(this.props.helpers, function(h) {
+                return !h.help_request;
+            });
+            elems.push(<button onClick={this.markLeft}>Student left</button>);
+            elems.push(
+                <form onSubmit={this.assignRequest}>
+                    <input type="submit">Assign to...</input>
+                    <select ref="helper">
+                        {_.map(availableHelpers, this.renderHelperOption)}
+                    </select>
+                </form>
             );
         }
 
