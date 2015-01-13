8e5674e0d554961c471435c43e6e4d16d198b26b
Omar Diab <o.s.diab@gmail.com>
2015-01-12 21:37:18 -0800

    only show feedback panel on helper view

--- src/HelperQueue.jsx
+++ src/HelperQueue.jsx
@@ -114,6 +114,13 @@ module.exports = React.createClass({
                         </div>
                     </div>
                 </div>
+                <footer className="row">
+                    <p className="col-sm-12 feedback-footer">
+                        Questions? Requests? Email me at <a href="mailto:osdiab@cs.stanford.edu">
+                            osdiab@cs.stanford.edu</a> or <a
+                            href="https://github.com/cs198/lair-queue/issues/new">
+                            file a Github issue</a>. Thanks!</p>
+                </footer>
             </div>
         );
     }
